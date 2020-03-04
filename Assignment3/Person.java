8358c86fde4592ad7d317751290ebbb0b767a8e7
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-03-03 19:41:25 -0800

    Update Person.java

--- Assignment3/Person.java
+++ Assignment3/Person.java
@@ -6,68 +6,7 @@
 
 package Exercise1;
 
-
-class Address
-{
-	private int streetNumber;
-	private String streetName;
-	private String city;
-	private int zipCode;
-	private String state;
-	
-	public void setStreetNumber(int streetNumber)
-	{
-		this.streetNumber = streetNumber;
-	}
-	public int getStreetNumber()
-	{
-		return streetNumber;
-	}
-	
-	public void setStreetName(String streetName)
-	{
-		this.streetName = streetName;
-	}
-	public String getStreetName()
-	{
-		return streetName;
-	}
-	
-	public void setCity(String city)
-	{
-		this.city = city;
-	}
-	public String getCity()
-	{
-		return city;
-	}
-	
-	public void setZipCode(int zipCode)
-	{
-		this.zipCode = zipCode;
-	}
-	public int getZipCode()
-	{
-		return zipCode;
-	}
-	
-	public void setState(String state)
-	{
-		this.state = state;
-	}
-	public String getState()
-	{
-		return state;
-	}
-	
-	 @Override
-	 public String toString() 
-	 {
-		return String.format(getStreetNumber() + " " + getStreetName() + ", " + getCity() + ", " + getZipCode() + ", " + getState() + ". ");
-	 }
-}
-
-public abstract class Person extends Address
+public class Person
 {
 	private String firstName;
 	private String lastName;
@@ -76,6 +15,11 @@ public abstract class Person extends Address
 	private Address address;
 	private String levelOfEducation;
 	private String specoalAccomo; //needs special accommodations or not
+	
+	public Person()
+	{
+		address = new Address();
+	}
 	
 	public Person(String firstName, String lastName, int age, int socialSecurityNumber)
 	{
@@ -125,21 +69,21 @@ public abstract class Person extends Address
 		 return specoalAccomo;
 	 }
 	 
-	 public String getAddress() 
+	 public Address getAddress() 
 	 {
-		return super.toString();
+		return address;
 	 }
 	 
 	 public void setAddress(Address address) 
 	 {
-		this.address = address;
-	 }	 
+		 this.address = address;
+	 }
 
 	 @Override
 	 public String toString() 
 	 {
 		 return String.format(getFirstName() + " " + getLastName() + ": " + getAge() + " years old, "+ "[The Social Security Number]: " +getSocialSecurityNumber()
-         +  ", [Address]: " + getAddress() + "\n" + "[Level of education]: " + getlevelOfEducation() + ", [Did needs special accommodations (Yes or not)]: " + getspecoalAccomo() + "\n");
+         + "\n" + "[Level of education]: " + getlevelOfEducation() + ", [Did needs special accommodations (Yes or not)]: " + getspecoalAccomo() +  "\n");
 	 }
 	 
 	 void introduce() 
