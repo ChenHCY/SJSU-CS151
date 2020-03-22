019b5cd32201bd99cac72f8b71bd3a8a922905e5
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-03-22 00:24:21 -0700

    Create ReadFileInput.java

---
+++ Assignment5/Exercise2/ReadFileInput.java
@@ -0,0 +1,35 @@
+//Student Name: Chenyu Yang
+//student ID: 014277029
+//Course: CS-151-2020SP
+//Lab: Homework 5 Exercise-2
+//Date: 2020/03/17
+
+package Exercise2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileInput 
+{
+	public static void main(String[] args) 
+	{
+		File myFile = null;
+		FileReader myFileReader = null;
+		String input = " ";
+		
+		try {
+			myFile = new File("quote.txt");
+			myFileReader = new FileReader(myFile);
+			BufferedReader reader = new BufferedReader(myFileReader);
+			
+			input = reader.readLine();
+			System.out.print(input);
+			
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+}
