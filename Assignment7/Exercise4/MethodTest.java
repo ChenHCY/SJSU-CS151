637a86ce9cc4986a4fc0968920124182df98cc7b
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-04-21 23:53:31 -0700

    Create MethodTest.java

---
+++ Assignment7/Exercise4/MethodTest.java
@@ -0,0 +1,16 @@
+//Student Name: Chenyu Yang
+//student ID: 014277029
+//Course: CS-151-2020SP
+//Lab: Homework 7 Exercise-4
+//Date: 2020/04/20
+
+package Exercise4;
+
+public class MethodTest 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
