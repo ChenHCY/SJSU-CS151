b024ec8bd59b3c4231986bc6beb5ec8c365a7cf9
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-04-21 23:22:54 -0700

    Create Shape.java

---
+++ Assignment7/Exercise2/Shape.java
@@ -0,0 +1,65 @@
+//Student Name: Chenyu Yang
+//student ID: 014277029
+//Course: CS-151-2020SP
+//Lab: Homework 7 Exercise-2
+//Date: 2020/04/20
+
+package Exercise2;
+
+import java.util.ArrayList;
+
+public abstract class Shape 
+{
+	static ArrayList<Shape> shapeList = new ArrayList<Shape>();
+
+	//the shapeList setter
+	public void setStringList(ArrayList<Shape> shapeList)
+	{
+	    this.shapeList = shapeList;
+	}
+	//the shapeList getter
+	public ArrayList<Shape> getShapeList() 
+	{
+	    return shapeList;
+	}
+
+	public void maxArea(ArrayList<Shape> shapeList)
+	{
+		Shape largest = null;
+		for(Shape shape: shapeList) 
+		{
+			if (largest == null || shape.computeArea() > largest.computeArea())
+			{
+				largest = shape;
+			}
+		}
+		 System.out.println("#### The details of the shape with largest area are :" + largest);		
+	}
+	
+	public void minArea(ArrayList<Shape> shapeList)
+	{
+		Shape smallest = null;
+		for(Shape shape: shapeList) 
+		{
+			if (smallest == null || shape.computeArea() < smallest.computeArea())
+			{
+				smallest = shape;
+			}
+		}
+		 System.out.println("#### The details of the shape with largest area are :" + smallest);		
+	}
+	
+    @Override
+    public String toString ()
+    {
+    	return String.format("[Shape Name]: ");
+    }
+    
+    public abstract double computeArea();
+    
+    public void compute()
+    {
+    	System.out.println(toString());
+
+    }
+}
