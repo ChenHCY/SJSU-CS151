6e2cb7ca18b4c4fdc9c4d06d420eed2395c7af63
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-04-23 11:10:21 -0700

    Update ShapeTest.java

--- Assignment7/Exercise1/ShapeTest.java
+++ Assignment7/Exercise1/ShapeTest.java
@@ -10,23 +10,24 @@ public class ShapeTest extends Shape
 {
 	public static void main(String[] args) 
 	{
+		Shape shape = new Shape();
 		//1. Triangle
-		shapeList.add(new Triangle(4.5,4));
-		shapeList.add(new Triangle(3,5.5));
+		shape.add(new Triangle(4.5,4));
+		shape.add(new Triangle(3,5.5));
 		
 		//2. Circle
-		shapeList.add (new Circle(5));
-		shapeList.add (new Circle(3.5));
+		shape.add (new Circle(5));
+		shape.add (new Circle(3.5));
 		
 		//3. Rectangle
-		shapeList.add (new Rectangle(5, 7));
-		shapeList.add (new Rectangle(3.5, 8.5));
+		shape.add (new Rectangle(5, 7));
+		shape.add (new Rectangle(3.5, 8.5));
 		
 		//4. Hexagon
-		shapeList.add (new Hexagon(8));
-		shapeList.add (new Hexagon(5.5));
-		shapeList.add (new Hexagon(4));
-		shapeList.remove (7);
+		shape.add (new Hexagon(8));
+		shape.add (new Hexagon(5.5));
+		shape.add (new Hexagon(4));
+		shape.remove (7);
 		
 		int i = 1;
 		for(Shape s : shapeList)
