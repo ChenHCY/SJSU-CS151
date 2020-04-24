46048f018f95d3bc8936137d459ebada6fff3bc7
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2020-04-23 23:13:40 -0700

    Add files via upload

---
+++ Assignment7/Exercise1/Shape.java
@@ -0,0 +1,12 @@
+//Student Name: Chenyu Yang
+//student ID: 014277029
+//Course: CS-151-2020SP
+//Lab: Homework 7 Exercise-1
+//Date: 2020/04/20
+
+package Exercise1;
+
+public interface Shape 
+{
+	Double computeArea();
+}
